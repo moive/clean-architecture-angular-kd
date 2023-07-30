32945799f1977534a5b5d301adc0598c3b2fb5d9
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2023-07-29 22:28:38 -0500

    get use case repository

--- src/presentation/app/app.component.ts
+++ src/presentation/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { UserLoginUseCase } from '../../domain/useCases/user-login.usercase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'clean-architecture-angular';
+
+  constructor(private userLoginUseCase: UserLoginUseCase) {}
+
+  getUser() {
+    this.userLoginUseCase
+      .execute({ username: '', password: '' })
+      .subscribe(console.log);
+  }
 }
